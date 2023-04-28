1457881a1a45d270d574a62d92425f943bb9cef1
teo3300 <matteo.rogora@live.it>
2023-04-28 15:04:09 +0200

    Better sway config
    Reofganizing sway config into different files, dividing them by type and
    properly formatting files to make them more readable, smaller statusbar config
    
    Signed-off-by: teo3300 <matteo.rogora@live.it>

--- sway/status.sh
+++ sway/status.sh
@@ -6,28 +6,6 @@ supply_info=$2
 # Get current date & time through and format
 time_date=$(date +"%a %Y-%m-%d %H:%M")
 
-# Get battery status (faster than "cat BAT1")
-if [[ -e $supply_info ]]; then
-    if [[ "$(cat $supply_info/online)" == "1"  ]]; then
-        charging=⚡
-    else
-        charging=🔋
-    fi
-else
-    charging=""
-fi
-
-# Get battery pergentage
-battery_percentage=$(expr $(expr $(cat $battery_info/charge_now) \* 100) / $(cat $battery_info/charge_full))
-
-# Get backlight value (0-100)
-backlight=$(brightnessctl g)
-
-bl_ico=""
-if [[ -n "$backlight" ]]; then
-    bl_ico="🔆"
-fi
-
 # Get available space in GiB (change -BG parameter)
 used_ram=$(free -h | grep Mem | awk '{print $3"/"$2}')
 
@@ -36,9 +14,6 @@ mic_stat=$(if [ "$(pamixer --default-source --get-mute)" = "true" ]; then echo -
 
 # Get Audio Volume & Mute
 speaker_stat=$(pamixer --get-volume)$(if [ "$(pamixer --get-mute)" = "true" ]; then echo -n "🔇"; else echo -n "🔊"; fi)
-
-# Get thermal info about Core_0
-thermal=$(sensors -u | grep Core -A1 | grep temp | awk '{print $2}' | sed 's/\..*//')
 
 # Get input method
 input_method=$(if [ "$(pgrep wlanthy)" ]; then
@@ -50,4 +25,4 @@ else
             head -n1 | tr -d '"')
 fi)
 
-echo $input_method🌐 $mic_stat$speaker_stat $used_ram💾 $backlight$bl_ico $battery_percentage$charging $thermal🌡️ $time_date
+echo $input_method🌐$'\t'$mic_stat$speaker_stat$'\t'$used_ram💾$'\t'$time_date
